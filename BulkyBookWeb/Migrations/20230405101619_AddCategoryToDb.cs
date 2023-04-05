680855616a9677150e1b8c05586e9b1022637a02
risha <risha@RISHABH008>
2023-04-06 00:26:48 +0530

    Add project files.

---
+++ BulkyBookWeb/Migrations/20230405101619_AddCategoryToDb.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBookWeb.Migrations
+{
+    public partial class AddCategoryToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MyProperty",
+                table: "Categories",
+                newName: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Categories",
+                newName: "MyProperty");
+        }
+    }
+}
